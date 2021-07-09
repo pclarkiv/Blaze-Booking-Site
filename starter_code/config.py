f3cf6e7b2b6404150a584fb0fd9e3e0a021da946
Paul Clark <paulclarkiv@yahoo.com>
2021-07-09 17:15:00 -0500

    initial deploy

--- starter_code/config.py
+++ starter_code/config.py
@@ -9,5 +9,5 @@ DEBUG = True
 # Connect to the database
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://paulclarkiv@localhost:5432/fyyur'
+SQLALCHEMY_DATABASE_URI = 'postgres://fhxlfxikvgzcyl:8d24f7a4391f7f186c469b979ef9a829da372f95c28e3b218958268a3ea3c77f@ec2-54-158-232-223.compute-1.amazonaws.com:5432/d33696mespvvav'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
